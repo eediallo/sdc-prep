cb8c0fe107a9d975c60be54f9850d64a7659b330
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-03-05 14:46:26 +0000

    refactor filter script to process a list of phrases and apply character inclusion

--- week4/python/filter.py
+++ week4/python/filter.py
@@ -1,9 +1,9 @@
 
-content = "This is a list of words"
+content = ['Go to the Gym', 'Go shopping', 'Call Michael', 'Attend meeting', 'Do homework', 'Give Demo']
 char = 'i'
 
 includesChar = lambda word: char in word
 
-filtered = filter(includesChar, content.split(' '))
+filtered = filter(includesChar, content)
 
 print(list(filtered))
