b5c9ea86fc153cdfc4f93cad44c10940dd708a0b
eediallo <elhhabdouldiallo@gmail.com>
2025-03-13 13:31:03 +0000

    add POST endpoint to handle quote submissions and refactor quote selection function

--- Decomposition/week1/backend/server.js
+++ Decomposition/week1/backend/server.js
@@ -14,15 +14,43 @@ const quotes = [
   },
 ];
 
-function pickRandomQuote() {
+function randomQuote() {
   const index = Math.floor(Math.random() * quotes.length);
   return quotes[index];
 }
 
 app.get("/", (req, res) => {
-  console.error("Received a request for a quote");
-  const quote = pickRandomQuote();
+    console.error('Request received from the client.')
+  const quote = randomQuote();
   res.send(`"${quote.quote}" -${quote.author}`);
+});
+
+app.post("/", (req, res) => {
+  const bodyBytes = [];
+  req.on("data", chunk => bodyBytes.push(...chunk));
+  req.on("end", () => {
+    const bodyString = String.fromCharCode(...bodyBytes);
+    console.log(bodyString, 'body string =====>')
+    let body;
+    try {
+      body = JSON.parse(bodyString);
+    } catch (error) {
+      console.error(`Failed to parse body ${bodyString} as JSON: ${error}`);
+      res.status(400).send("Expected body to be JSON.");
+      return;
+    }
+    if (typeof body != "object" || !("quote" in body) || !("author" in body)) {
+      console.error(`Failed to extract quote and author from post body: ${bodyString}`);
+      res.status(400).send("Expected body to be a JSON object containing keys quote and author.");
+      return;
+    }
+    quotes.push({
+      quote: body.quote,
+      author: body.author,
+    });
+    console.log(quotes)
+    res.send("ok");
+  });
 });
 
 app.listen(port, () => {
